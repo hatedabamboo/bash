4018ff77b4e5105a60460c340ca1420329733513
hatedabamboo <k.solovey@corp.vk.com>
2019-10-09 19:58:00 +0300

    Move token to getid_config

--- getid.py
+++ getid.py
@@ -2,10 +2,11 @@
 
 import vk
 import sys
+import getid_config
 
 session = vk.Session()
 api = vk.API(session, v = 5.102)
-token = '# YOUR API TOKEN HERE #'
+token = getid_config.token
 
 userid = sys.argv[1]
 r = api.users.get(access_token = token, user_ids = userid)
