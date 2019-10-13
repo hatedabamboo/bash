961bc4af20b4c6dac28b24edbc7dc38fa5564368
hatedabamboo <k.solovey@corp.vk.com>
2019-10-13 07:39:54 +0300

    Add some codestyle lines

--- getid.py
+++ getid.py
@@ -4,9 +4,11 @@ import vk
 import sys
 import getid_config
 
+
 session = vk.Session()
 api = vk.API(session, v = 5.102)
 token = getid_config.token
+
 
 if len(sys.argv) == 1:
     print('Error: missing short name.')
